11cfd2180d8758918c0f4ba1b1dda57ac558b4a5
nicothebrush <nicola.riolini@gmail.com>
2020-02-21 17:25:57 +0100

    generic bugfix (fast message)

--- edi_etl_data/script/etl/customer.py
+++ edi_etl_data/script/etl/customer.py
@@ -82,7 +82,7 @@ print('Read Customer CSV file: %s' % customer_csv)
 file_data = open(customer_csv, 'r')
 import pdb; pdb.set_trace()
 for line in file_data:
-    print line
+    print(line)
     continue
     
     i += 1
