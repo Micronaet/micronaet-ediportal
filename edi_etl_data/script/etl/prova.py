dae635a26c9f34c3c674a69a7ef6eb576910dd03
nicothebrush <nicola.riolini@gmail.com>
2020-02-21 17:29:08 +0100

    generic bugfix (fast message)

--- edi_etl_data/script/etl/prova.py
+++ edi_etl_data/script/etl/prova.py
@@ -1,7 +1,6 @@
 # Copyright 2019  Micronaet SRL (<http://www.micronaet.it>).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-print('Read Customer CSV file: %s' % customer_csv)
 customer_csv = '/home/odoo/script/etl/data/testdestgfd.csv'
 
 file_data = open(customer_csv, 'r')
