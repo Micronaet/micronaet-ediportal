2f7aae4abb8b81631c6142a66705cf126e634405
nicothebrush <nicola.riolini@gmail.com>
2020-02-21 17:25:09 +0100

    generic bugfix (fast message)

--- edi_etl_data/script/etl/customer.py
+++ edi_etl_data/script/etl/customer.py
@@ -78,7 +78,6 @@ pricelist_pool = odoo.model('res.partner.pricelist')
 i = 0
 print('Read Customer CSV file: %s' % customer_csv)
 
-import pdb; pdb.set_trace()
 #file_data = pandas.read_csv(customer_csv, encoding='unicode_escape')
 file_data = open(customer_csv, 'r')
 import pdb; pdb.set_trace()
