20d70b3769139dec847b84e4884aa3b1f5baedf3
nicothebrush <nicola.riolini@gmail.com>
2020-02-26 15:25:53 +0100

    generic bugfix (fast message)

--- edi_etl_data/models/edi_data.py
+++ edi_etl_data/models/edi_data.py
@@ -129,5 +129,5 @@ class SaleOrder(models.Model):
     # -------------------------------------------------------------------------
     user_id = fields.Many2one('res.users', 'Customer user')
     my_line_ids = fields.One2many('sale.order.line.my', 'order_id', 'My line')
-    portal_partner_id = fields.Many2one(
-        'res.partner', 'Portal partner', related='user_id.portal_partner_id')
+    partner_id = fields.Many2one(
+        'res.partner', 'Partner', related='user_id.portal_partner_id')
